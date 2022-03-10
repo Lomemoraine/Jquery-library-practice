5a91cc1739a1558b1070ea8045deb7c23c804676
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-03-10 14:33:15 +0300

    add a toggle  button in html file and a query it from the jquery

---
+++ index.js
@@ -0,0 +1,8 @@
+// Jquery effects 1.Show effect
+$(document).ready(function() {
+    $("button").click(function() {
+        $("img").toggle();
+    
+    });
+  });
+// toggle method  toggles between hide and show
